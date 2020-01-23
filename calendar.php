00e8861e56ac9cb4473ab1f2f68cea5b01f7b66c
Bas van der Vlies <basvandervlies@hotmail.com>
2020-01-23 11:56:51 +0100

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -29,8 +29,8 @@ if(isset($_GET["id"]) && isset($_GET["api"])){
   curl_close($curl);
   $data = json_decode($response);
   // Maak het downloadable
-  //header('Content-type: text/calendar; charset=utf-8');
-  //header('Content-Disposition: attachment; filename=SAH_cal.ics'); 
+  header('Content-type: text/calendar; charset=utf-8');
+  header('Content-Disposition: attachment; filename=SAH_cal.ics'); 
   // Begin van ICS file?>
 BEGIN:VCALENDAR
 VERSION:2.0
@@ -58,35 +58,35 @@ Not working yet
 
 */
 
-$curl = curl_init();
+// $curl = curl_init();
 
-  curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://z0shy2ecl0.execute-api.eu-west-1.amazonaws.com/production/customer",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_HTTPHEADER => array(
-      "accept:  application/json, text/plain, */*",
-      "customerusername: ". $customerUsername,
-      "idtoken:  ". $_GET["id"],
-      "x-api-key:  ". $_GET["api"]
-      ),
-  ));
+//   curl_setopt_array($curl, array(
+//     CURLOPT_URL => "https://z0shy2ecl0.execute-api.eu-west-1.amazonaws.com/production/customer",
+//     CURLOPT_RETURNTRANSFER => true,
+//     CURLOPT_ENCODING => "",
+//     CURLOPT_MAXREDIRS => 10,
+//     CURLOPT_TIMEOUT => 0,
+//     CURLOPT_FOLLOWLOCATION => true,
+//     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//     CURLOPT_CUSTOMREQUEST => "GET",
+//     CURLOPT_HTTPHEADER => array(
+//       "accept:  application/json, text/plain, */*",
+//       "customerusername: ". $customerUsername,
+//       "idtoken:  ". $_GET["id"],
+//       "x-api-key:  ". $_GET["api"]
+//       ),
+//   ));
 
-  $customerJson = curl_exec($curl);
-  curl_close($curl);
+//   $customerJson = curl_exec($curl);
+//   curl_close($curl);
 
-  $customerInfo = json_decode($customerJson);
+//   $customerInfo = json_decode($customerJson);
 
-  foreach($customerInfo->user as $value1){
-    $givenName = $value1->attributes->given_name;
-    $middleName = $value1->attributes->middle_name . " ";
-    $familyName = $value1->attributes->family_name;
-  }
+//   foreach($customerInfo->user as $value1){
+//     $givenName = $value1->attributes->given_name;
+//     $middleName = $value1->attributes->middle_name . " ";
+//     $familyName = $value1->attributes->family_name;
+//   }
 
 /* 
 
